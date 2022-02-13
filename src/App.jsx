0e890ef9da67a39720e62489db3ee7e3821b651c
Dwayne Williams <dwaynewillmakeit@gmail.com>
2022-02-13 02:20:03 -0500

    Create about page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import FeedbackStats from "./components/FeedbackStats";
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 import { v4 as uuidv4 } from "uuid";
+import AboutPage from "./pages/AboutPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AboutIconLink from "./components/AboutIconLink";
 
 const App = () => {
 	const [feedback, setFeedback] = useState(FeedbackData);
@@ -21,14 +24,29 @@ const App = () => {
 	};
 
 	return (
-		<>
+		<Router>
 			<Header />
 			<div className="container">
-				<FeedbackForm handleAdd={addFeedback} />
-				<FeedbackStats feedback={feedback} />
-				<FeedBackList feedback={feedback} handleDelete={deleteFeedback} />
+				<Routes>
+					<Route
+						exact
+						path="/"
+						element={
+							<>
+								<FeedbackForm handleAdd={addFeedback} />
+								<FeedbackStats feedback={feedback} />
+								<FeedBackList
+									feedback={feedback}
+									handleDelete={deleteFeedback}
+								/>
+								<AboutIconLink />
+							</>
+						}
+					></Route>
+					<Route path="/about" element={<AboutPage />} />
+				</Routes>
 			</div>
-		</>
+		</Router>
 	);
 };
 
